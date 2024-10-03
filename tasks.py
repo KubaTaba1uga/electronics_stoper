d4a27114358e97707d7cb35b7997037f186e774b
KubaTaba1uga <luna7337anul@proton.me>
2024-10-03 05:33:59 +0000

    add kicad project to repo

--- tasks.py
+++ tasks.py
@@ -1,17 +1,27 @@
 ###############################################
 #                Imports                      #
 ###############################################
+import logging
 import os
-import subprocess
+import shutil
+import sys
+import traceback
 
-from invoke import task
+from invoke import UnexpectedExit, task
 
 ###############################################
 #                Public API                   #
 ###############################################
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 FRITIZNG_PATH = os.path.join(ROOT_PATH, "fritizing")
-CC = "openscad"
+QUCS_PATH = os.path.join(ROOT_PATH, "qucs")
+KICAD_PATH = os.path.join(ROOT_PATH, "kicad")
+
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 @task
@@ -19,5 +29,75 @@ def open_breadboard_view(c):
     """
     Opens the Fritzing application for a breadboard view.
     """
-    fritzing_command = ["fritzing", os.path.join(FRITIZNG_PATH, "breadboard.fzz")]
-    subprocess.run(fritzing_command)
+    command = ["fritzing", os.path.join(FRITIZNG_PATH, "breadboard.fzz")]
+
+    run_command(c, command)
+
+
+@task
+def open_qucs_view(c):
+    """
+    Opens the QUCS application for a schematic view.
+    """
+    command = ["qucs-s", os.path.join(QUCS_PATH, "stoper.sch")]
+
+    run_command(c, command)
+
+
+@task
+def open_kicad_view(c):
+    """
+    Opens the QUCS application for a schematic view.
+    """
+    command = ["kicad", os.path.join(KICAD_PATH, "stoper.kicad_pro")]
+
+    run_command(c, command)
+
+
+###############################################
+#                Pivate API                   #
+###############################################
+
+
+def command_exists(command):
+    """
+    Check if a command exists in the system.
+
+    :param command: Command name as a string.
+    :return: True if the command exists, False otherwise.
+    """
+    return shutil.which(command) is not None
+
+
+def run_command(process, command_l):
+    """
+    Execute a shell command using the given process and handle errors.
+
+    Joins the command list `command_l` into a string, checks if the command exists,
+    and executes it using `process.run()`. Logs an error and exits if the command
+    is not found or fails to execute.
+
+    Args:
+        process (invoke.context.Context): The process object used to execute the command.
+        command_l (list of str): The command and its arguments as a list of strings.
+
+    Returns:
+        None: Logs errors and exits on failure.
+
+    Raises:
+        SystemExit: Exits with status 1 if the command is not found, or status 2 if execution fails.
+    """
+
+    command = " ".join(command_l)
+
+    logging.debug("Executing `%s`." % command)
+
+    if not command_exists(command_l[0]):
+        logging.error("Unable to find `%s` app." % command)
+        sys.exit(1)
+
+    try:
+        process.run(command)
+    except UnexpectedExit:
+        logging.error("Unable to run `%s`: %s" % (command, traceback.format_exc()))
+        sys.exit(2)
